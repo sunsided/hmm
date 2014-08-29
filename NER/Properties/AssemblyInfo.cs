6e6dfdf1516cc5ae02051b5cb88ceefa756a4496
Markus Mayer <markus.mayer@derixx.de>
2014-08-29 19:14:22 +0200

    renamed from NER to HMM

--- NER/Properties/AssemblyInfo.cs
+++ NER/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NER")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Hidden Markov Model")]
+[assembly: AssemblyDescription("Hidden Markov Model tests")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NER")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Hidden Markov Model")]
+[assembly: AssemblyCopyright("Copyright © Markus Mayer 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
